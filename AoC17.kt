5f9714be7497a7ac7b210ec4578a359297567674
Tomáš Vikiszály <111954979+TomasTatra@users.noreply.github.com>
2023-12-18 07:56:45 +0100

    Add files via upload

---
+++ AoC17.kt
@@ -0,0 +1,103 @@
+import java.io.File
+import java.io.InputStream
+import java.lang.IndexOutOfBoundsException
+
+val direction = listOf(Pair(-1, 0), Pair(0, -1), Pair(1, 0), Pair(0, 1))
+
+fun main() {
+    val inputStream: InputStream = File("example.txt").inputStream()
+    val strings = inputStream.bufferedReader().use { it.readText().split("\r\n") }
+    val map = strings.map { e -> e.map { k -> k.digitToInt() } }
+    val graph = strings.map { e -> e.map { _ -> mutableListOf<MutableList<Int>>() }.toMutableList() }.toMutableList()
+    graph[0][0].add(listOf(0, 0, 0, 0).toMutableList())
+    val input = mutableListOf(Pair(0, 0))
+    val end = Pair(map.lastIndex, map.last().lastIndex)
+    while (input.isNotEmpty()) {
+        val position = input.removeAt(0)
+        if (position == end)
+            continue
+        for (way in direction) {
+            val newPosition = Pair(position.first + way.first, position.second + way.second)
+            val value: Int
+            try {
+                value = map[newPosition.first][newPosition.second]
+            } catch (_: IndexOutOfBoundsException) {
+                continue
+            }
+            for (set in graph[position.first][position.second]) {
+                val oldValue = set[0]
+                val last = Pair(set[1], set[2])
+                val oneWay = set[3]
+                if (oneWay > 10)
+                    continue
+                if (last != Pair(0, 0))
+                    if (oneWay < 4 && last != way)
+                        continue
+                if (last.first * -1 == way.first && last.second * -1 == way.second)
+                    continue
+                val newValue = oldValue + value
+                var find = false
+                val copy = graph[newPosition.first][newPosition.second]
+                for (secondSet in copy) {
+                    val secondValue = secondSet[0]
+                    val secondLast = Pair(secondSet[1], secondSet[2])
+                    val secondOneWay = secondSet[3]
+                    if (last == way) {
+                        if (secondLast == way && secondOneWay == oneWay + 1) {
+                            if (secondValue > newValue) {
+                                graph[newPosition.first][newPosition.second].remove(secondSet)
+                                graph[newPosition.first][newPosition.second].add(
+                                    mutableListOf(
+                                        newValue, way.first, way.second, secondOneWay
+                                    )
+                                )
+                                if (secondOneWay > 3 && !input.contains(newPosition))
+                                    input.add(newPosition)
+                            }
+                            find = true
+                            break
+                        }
+                    } else {
+                        if (secondLast == way && secondOneWay == 1) {
+                            if (secondValue > newValue) {
+                                graph[newPosition.first][newPosition.second].remove(secondSet)
+                                graph[newPosition.first][newPosition.second].add(
+                                    mutableListOf(
+                                        newValue, way.first, way.second, 1
+                                    )
+                                )
+                                if (!input.contains(newPosition))
+                                    input.add(newPosition)
+                            }
+                            find = true
+                            break
+                        }
+                    }
+                }
+                if (!find) {
+                    if (!input.contains(newPosition))
+                        input.add(newPosition)
+                    if (last == way) {
+                        if (oneWay < 10)
+                            graph[newPosition.first][newPosition.second].add(
+                                mutableListOf(
+                                    newValue, way.first, way.second, oneWay + 1
+                                )
+                            )
+                    } else
+                        graph[newPosition.first][newPosition.second].add(
+                            mutableListOf(
+                                newValue, way.first, way.second, 1
+                            )
+                        )
+                }
+            }
+        }
+    }
+    var min = Int.MAX_VALUE
+    for (m in graph[end.first][end.second]) {
+        if (m[0] < min && m[3] > 3)
+            min = m[0]
+    }
+    println(min)
+}
---
+++ AoC18.kt
@@ -0,0 +1,47 @@
+import java.io.File
+import java.io.InputStream
+import kotlin.math.abs
+
+val mapOfMove = mapOf(0 to Pair(0, 1), 2 to Pair(0, -1), 1 to Pair(1, 0), 3 to Pair(-1, 0))
+
+fun main() {
+    val inputStream: InputStream = File("example.txt").inputStream()
+    val strings = inputStream.bufferedReader().use { it.readText().split("\r\n") }
+
+    val graph = mutableListOf<List<Long>>()
+    var positionX:Long = 0
+    var positionY:Long = 0
+    var distance:Long = 0
+    for (string in strings) {
+        val instruction = string.split(" ")
+        val color = instruction[2].subSequence(1, instruction[2].length - 1).toString()
+        val direction = color.last().toString().toInt()
+        val long = color.subSequence(1, 6).toString().toInt(16)
+        val newPositionX:Long = positionX + mapOfMove[direction]!!.first * long
+        val newPositionY:Long = positionY + mapOfMove[direction]!!.second * long
+        graph.add(listOf(positionX, positionY))
+        positionX = newPositionX
+        positionY = newPositionY
+        distance += long
+    }
+    println(dig(graph) + distance.shr(1) + 1)
+}
+
+fun dig(graph: MutableList<List<Long>>): Long {
+    var sum:Long = 0
+    for (index in graph.indices) {
+        val x1 = graph[index][0]
+        val y1 = graph[index][1]
+        val x2: Long
+        val y2: Long
+        if (index != graph.lastIndex) {
+            x2 = graph[index + 1][0]
+            y2 = graph[index + 1][1]
+        } else {
+            x2 = graph[0][0]
+            y2 = graph[0][1]
+        }
+        sum += (x1 * y2 - x2 * y1)
+    }
+    return abs(sum).shr(1)
+}
